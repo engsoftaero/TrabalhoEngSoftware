18729f45505a7bfdb7d68b4f15c57f0305cf2d10
Nickolas Victor <nickolasvictor@hotmail.com>
2019-03-29 18:32:55 -0300

    Confirmar Tudo

---
+++ EngSoftwareForum/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EngSoftwareForum.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
