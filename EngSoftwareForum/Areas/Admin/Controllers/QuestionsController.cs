daebe4a78ff05a98fa56c1dd2e408d9c26b83250
Nickolas Victor <nickolasvictor@hotmail.com>
2019-04-01 19:34:37 -0300

    Commit

--- EngSoftwareForum/Areas/Admin/Controllers/QuestionsController.cs
+++ EngSoftwareForum/Areas/Admin/Controllers/QuestionsController.cs
@@ -47,5 +47,84 @@ namespace EngSoftwareForum.Areas.Admin.Controllers
             }
             return View(question_par);
         }
+
+        //GET - EDIT
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var question_par = await _db.Questions.FindAsync(id);
+            if (question_par == null)
+            {
+                return NotFound();
+            }
+            return View(question_par);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Questions question_par)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Update(question_par);
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(question_par);
+        }
+
+
+        //GET - DELETE
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var question_par = await _db.Questions.FindAsync(id);
+            if (question_par == null)
+            {
+                return NotFound();
+            }
+            return View(question_par);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            var questio_par = await _db.Questions.FindAsync(id);
+
+            if (questio_par == null)
+            {
+                return View();
+            }
+
+            _db.Questions.Remove(questio_par);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        //GET - DETAILS
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var question_par = await _db.Questions.FindAsync(id);
+            if (question_par == null)
+            {
+                return NotFound();
+            }
+            return View(question_par);
+        }
+
     }
 }
